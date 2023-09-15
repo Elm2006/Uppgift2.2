951d9d62e395559de89d57a4c70e7a5309447cee
isel06002 <isel06002@E-PW0234HX>
2023-09-15 12:31:52 +0200

    Add project files.

---
+++ Uppgift2.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift2.__2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur långt hoppade Elin i meter?");
+            double Elin =double. Parse(Console.ReadLine());
+            Console.WriteLine("Hur långt hoppade Alma i meter");
+            double Alma =double.Parse(Console.ReadLine());
+            double dif= Elin - Alma;
+            Console.WriteLine("Elin hoppade " + dif + "meter högre än Alma");
+        }
+    }
+}
